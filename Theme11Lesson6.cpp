1cf38f47b8e15ec6dcc13cfc92f98ad1c8c4bb2f
XenoneX <amb@bard.su>
2022-04-25 18:00:10 +0300

    Добил таки.

---
+++ Theme11Lesson6.cpp
@@ -0,0 +1,201 @@
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+enum class QueryType {
+    NewBus,
+    BusesForStop,
+    StopsForBus,
+    AllBuses,
+};
+
+struct Query {
+    QueryType type;
+    string bus;
+    string stop;
+    vector<string> stops;
+};
+
+string implode(const vector<string> &elems, const string &delim) {
+    string s;
+    for (auto ii = elems.begin(); ii != elems.end(); ++ii) {
+        s += (*ii);
+        if (ii + 1 != elems.end()) {
+            s += delim;
+        }
+    }
+
+    return s;
+}
+
+istream &operator>>(istream &is, Query &q) {
+    // Реализуйте эту функцию
+    string operation_code;
+    is >> operation_code;
+    if (operation_code == "NEW_BUS"s) {
+        q.type = QueryType::NewBus;
+        is >> q.bus;
+        int stop_count;
+        is >> stop_count;
+        q.stops.resize(stop_count);
+        for (string &stop: q.stops) {
+            is >> stop;
+        }
+    } else if (operation_code == "BUSES_FOR_STOP"s) {
+        q.type = QueryType::BusesForStop;
+        is >> q.stop;
+    } else if (operation_code == "STOPS_FOR_BUS"s) {
+        q.type = QueryType::StopsForBus;
+        is >> q.bus;
+    } else if (operation_code == "ALL_BUSES"s) {
+        q.type = QueryType::AllBuses;
+    }
+    return is;
+}
+
+struct BusesForStopResponse {
+    // Наполните полями эту структуру
+    vector<string> buses;
+};
+
+ostream &operator<<(ostream &os, const BusesForStopResponse &r) {
+    // Реализуйте эту функцию
+    if (r.buses.empty())
+        os << "No stop";
+    else
+        os << implode(r.buses, " ");
+    return os;
+}
+
+struct StopsForBusResponse {
+    // Наполните полями эту структуру
+    vector<string> stops;
+    map<string, vector<string>> stops_to_bus;
+};
+
+ostream &operator<<(ostream &os, const StopsForBusResponse &r) {
+    // Реализуйте эту функцию
+    if (r.stops.empty())
+        os << "No bus";
+    else {
+        for (const string &stop: r.stops) {
+            os << "Stop " << stop << ": ";
+            if (r.stops_to_bus.at(stop).empty())
+                os << "no interchange";
+            else
+                os << implode(r.stops_to_bus.at(stop), " ");
+            if (stop != *(r.stops.end() - 1))
+                os << endl;
+        }
+    }
+    return os;
+}
+
+struct AllBusesResponse {
+    map<string, vector<string>> lines;
+};
+
+ostream &operator<<(ostream &os, const AllBusesResponse &r) {
+    // Реализуйте эту функцию
+    if (r.lines.empty())
+        os << "No buses";
+    else {
+        vector<string> buses;
+        for (const auto &[bus, stops]: r.lines) {
+            buses.push_back(bus);
+        }
+        for (auto ii = buses.begin(); ii != buses.end(); ++ii) {
+            os << "Bus " << *(ii) << ": ";
+            os << implode(r.lines.at(*(ii)), " ");
+            if (ii != (buses.end() - 1))
+                os << endl;
+        }
+    }
+    return os;
+}
+
+class BusManager {
+public:
+    void AddBus(const string &bus, const vector<string> &stops) {
+        lines_[bus] = stops;
+        buses_.push_back(bus);
+    }
+
+    BusesForStopResponse GetBusesForStop(const string &stop) const {
+        // Реализуйте этот метод
+        BusesForStopResponse result;
+        for (const auto& bus: buses_)
+            if (count(lines_.at(bus).begin(), lines_.at(bus).end(), stop)) result.buses.push_back(bus);
+        return result;
+    }
+
+    StopsForBusResponse GetStopsForBus(const string &bus) const {
+        // Реализуйте этот метод
+        StopsForBusResponse result;
+        if (lines_.count(bus))
+            result.stops = lines_.at(bus);
+        for (const string &stop: result.stops) {
+            vector<string> r;
+            for (const string &cur_bus: GetBusesForStop(stop).buses)
+                if (cur_bus != bus) r.push_back(cur_bus);
+            result.stops_to_bus[stop] = r;
+        }
+        return result;
+    }
+
+    AllBusesResponse GetAllBuses() const {
+        // Реализуйте этот метод
+        AllBusesResponse result;
+        result.lines = lines_;
+        return result;
+    }
+
+private:
+    map<string, vector<string>> lines_;
+    vector<string> buses_;
+};
+
+// Не меняя тела функции main, реализуйте функции и классы выше
+
+void TestAddNewBusInput() {
+    istringstream input;
+    input.str("NEW_BUS 32 3 Tolstopaltsevo Marushkino Vnukovo"s);
+
+    Query q;
+    input >> q;
+    assert(q.type == QueryType::NewBus);
+    assert(q.bus == "32");
+    assert(q.stops.size() == 3);
+}
+
+int main() {
+    int query_count;
+    Query q;
+
+    cin >> query_count;
+
+    BusManager bm;
+    for (int i = 0; i < query_count; ++i) {
+        cin >> q;
+        switch (q.type) {
+            case QueryType::NewBus:
+                bm.AddBus(q.bus, q.stops);
+                break;
+            case QueryType::BusesForStop:
+                cout << bm.GetBusesForStop(q.stop) << endl;
+                break;
+            case QueryType::StopsForBus:
+                cout << bm.GetStopsForBus(q.bus) << endl;
+                break;
+            case QueryType::AllBuses:
+                cout << bm.GetAllBuses() << endl;
+                break;
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,185 +1,58 @@
-#include <cassert>
-#include <iostream>
 #include <algorithm>
+#include <cassert>
+#include <cmath>
 #include <map>
+#include <set>
 #include <string>
+#include <utility>
 #include <vector>
-#include <sstream>
 
 using namespace std;
 
-enum class QueryType {
-    NewBus,
-    BusesForStop,
-    StopsForBus,
-    AllBuses,
-};
+/* Подставьте вашу реализацию класса SearchServer сюда */
 
-struct Query {
-    QueryType type;
-    string bus;
-    string stop;
-    vector<string> stops;
-};
+// -------- Начало модульных тестов поисковой системы ----------
 
-istream &operator>>(istream &is, Query &q) {
-    // Реализуйте эту функцию
-    string operation_code;
-    is >> operation_code;
-    if (operation_code == "NEW_BUS"s) {
-        q.type = QueryType::NewBus;
-        is >> q.bus;
-        int stop_count;
-        is >> stop_count;
-        q.stops.resize(stop_count);
-        for (string &stop: q.stops) {
-            is >> stop;
-        }
-    } else if (operation_code == "BUSES_FOR_STOP"s) {
-        q.type = QueryType::BusesForStop;
-        is >> q.stop;
-    } else if (operation_code == "STOPS_FOR_BUS"s) {
-        q.type = QueryType::StopsForBus;
-        is >> q.bus;
-    } else if (operation_code == "ALL_BUSES"s) {
-        q.type = QueryType::AllBuses;
+// Тест проверяет, что поисковая система исключает стоп-слова при добавлении документов
+void TestExcludeStopWordsFromAddedDocumentContent() {
+    const int doc_id = 42;
+    const string content = "cat in the city"s;
+    const vector<int> ratings = {1, 2, 3};
+    // Сначала убеждаемся, что поиск слова, не входящего в список стоп-слов,
+    // находит нужный документ
+    {
+        SearchServer server;
+        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        const auto found_docs = server.FindTopDocuments("in"s);
+        assert(found_docs.size() == 1);
+        const Document& doc0 = found_docs[0];
+        assert(doc0.id == doc_id);
     }
-    return is;
+
+    // Затем убеждаемся, что поиск этого же слова, входящего в список стоп-слов,
+    // возвращает пустой результат
+    {
+        SearchServer server;
+        server.SetStopWords("in the"s);
+        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        assert(server.FindTopDocuments("in"s).empty());
+    }
 }
 
-struct BusesForStopResponse {
-    // Наполните полями эту структуру
-    vector<string> buses;
-};
+/*
+Разместите код остальных тестов здесь
+*/
 
-ostream &operator<<(ostream &os, const BusesForStopResponse &r) {
-    // Реализуйте эту функцию
-    if (r.buses.empty())
-        os << "No stop";
-    else
-        for (const string &bus: r.buses)
-            os << bus << " ";
-    return os;
+// Функция TestSearchServer является точкой входа для запуска тестов
+void TestSearchServer() {
+    TestExcludeStopWordsFromAddedDocumentContent();
+    // Не забудьте вызывать остальные тесты здесь
 }
 
-struct StopsForBusResponse {
-    // Наполните полями эту структуру
-    vector<string> stops;
-    map<string, vector<string>> stops_to_bus;
-};
-
-ostream &operator<<(ostream &os, const StopsForBusResponse &r) {
-    // Реализуйте эту функцию
-    if (r.stops.empty())
-        os << "No bus";
-    else {
-        for (const string &stop: r.stops) {
-            os << "Stop " << stop << ": ";
-            if (r.stops_to_bus.at(stop).empty())
-                os << "no interchange";
-            else
-                for (const string &bus: r.stops_to_bus.at(stop))
-                    os << bus << " ";
-            os << endl;
-        }
-    }
-    return os;
-}
-
-struct AllBusesResponse {
-    map<string, vector<string>> lines;
-};
-
-ostream &operator<<(ostream &os, const AllBusesResponse &r) {
-    // Реализуйте эту функцию
-    if (r.lines.empty())
-        os << "No buses";
-    else
-        for (const auto& [bus, stops] : r.lines)
-        {
-            os << "Bus " << bus << ": ";
-            for (const string& stop : stops)
-                os << stop << " ";
-            os << endl;
-        }
-    return os;
-}
-
-class BusManager {
-public:
-    void AddBus(const string &bus, const vector<string> &stops) {
-        lines_[bus] = stops;
-    }
-
-    BusesForStopResponse GetBusesForStop(const string &stop) const {
-        // Реализуйте этот метод
-        BusesForStopResponse result;
-        for (const auto &[bus, stops]: lines_)
-            if (count(stops.begin(), stops.end(), stop)) result.buses.push_back(bus);
-        return result;
-    }
-
-    StopsForBusResponse GetStopsForBus(const string &bus) const {
-        // Реализуйте этот метод
-        StopsForBusResponse result;
-        if (lines_.count(bus))
-            result.stops = lines_.at(bus);
-        for (const string &stop: result.stops)
-        {
-            vector<string> r;
-            for (const string& cur_bus : GetBusesForStop(stop).buses)
-                if (cur_bus != bus) r.push_back(cur_bus);
-            result.stops_to_bus[stop] = r;
-        }
-        return result;
-    }
-
-    AllBusesResponse GetAllBuses() const {
-        // Реализуйте этот метод
-        AllBusesResponse result;
-        result.lines = lines_;
-        return result;
-    }
-
-private:
-    map<string, vector<string>> lines_;
-};
-
-// Не меняя тела функции main, реализуйте функции и классы выше
-
-void TestAddNewBusInput() {
-    istringstream input;
-    input.str("NEW_BUS 32 3 Tolstopaltsevo Marushkino Vnukovo"s);
-
-    Query q;
-    input >> q;
-    assert(q.type == QueryType::NewBus);
-    assert(q.bus == "32");
-    assert(q.stops.size() == 3);
-}
+// --------- Окончание модульных тестов поисковой системы -----------
 
 int main() {
-    int query_count;
-    Query q;
-
-    cin >> query_count;
-
-    BusManager bm;
-    for (int i = 0; i < query_count; ++i) {
-        cin >> q;
-        switch (q.type) {
-            case QueryType::NewBus:
-                bm.AddBus(q.bus, q.stops);
-                break;
-            case QueryType::BusesForStop:
-                cout << bm.GetBusesForStop(q.stop) << endl;
-                break;
-            case QueryType::StopsForBus:
-                cout << bm.GetStopsForBus(q.bus) << endl;
-                break;
-            case QueryType::AllBuses:
-                cout << bm.GetAllBuses() << endl;
-                break;
-        }
-    }
+    TestSearchServer();
+    // Если вы видите эту строку, значит все тесты прошли успешно
+    cout << "Search server testing finished"s << endl;
 }
